52c13e428294fbba8d22a4feec8eb15a2ca686b7
zarden <y1352366024@yeah.net>
2019-09-03 10:48:45 +0800

    ClassObject3

--- ClassObject3/ClassObject3/main.cpp
+++ ClassObject3/ClassObject3/main.cpp
@@ -1,39 +1,46 @@
 #include<iostream>
-
 using namespace std;
 
 class Test2{
 
-
 public:
-	Test2(int i)
-	{_
-	}
+	int b;
+	
+	explicit Test2(int a) :b(a)
+	{}
+
+	Test2(Test2 & t)
+	{
+		b = t.b;
+	}	
 };
-
-
 
 class Test
 {
 public:
-	const int i;
+	int a = 5;
+
+	const int i=3;
+
 	int &ri;
 	//auto  不行
+	Test2 t2;
 
 	Test(int &a) :i(3), ri(a),t2(a)//相当于初始化
 	{}
 };
 
-
-
-
-
-
-
-
-int main()
+int main1()
 {
-	
+	int i = 8;
+	int j = 9;
+	Test2 a(i);
+	Test2 b = a;
+
+	Test2 c(j);
+
+	cout << a.b << endl;//8
+	cout << c.b << endl;//9
 
 	system("pause");
 	return 0;

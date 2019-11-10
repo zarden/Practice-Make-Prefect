f60333b237f34eb38757403f8dfecc8d7bde5ee6
zarden <y1352366024@yeah.net>
2019-11-10 22:30:29 +0800

    findNumSet

---
+++ findString/findString/test.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int g_blbitmap = 0;
+int g_slbitmap = 0;
+
+void setbit(char ch)
+{
+	if (ch<='Z' && ch>='A')
+	{
+		g_blbitmap |= 1 << ch - 'A';
+	}
+	else
+	{
+		g_slbitmap |= 1 << ch - 'a';
+	}
+}
+
+bool find(char ch)
+{
+	if (ch <= 'Z' && ch >= 'A')
+	{
+		return (g_blbitmap >> ch - 'A')&1;
+	}
+	else
+	{
+		return (g_slbitmap >> ch - 'a') & 1;
+	}
+}
+
+int main()
+{
+	char tmp[101] = { 0 };
+	cin >> tmp;
+	string src(tmp);
+	string dst;
+
+	for (auto &ch : src)
+	{
+		if (!find(ch))
+		{
+			dst.push_back(ch);
+			setbit(ch);
+		}
+	}
+	cout << dst;
+	system("pause");
+	return 0;
+}

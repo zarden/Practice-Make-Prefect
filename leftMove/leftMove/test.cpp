0d090c2ee4db202a510eb5c7e49abeaeb64807c7
zarden <y1352366024@yeah.net>
2019-11-15 22:35:36 +0800

    leftMove

---
+++ leftMove/leftMove/test.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+public:	int m_b;
+};
+class Test:public Base
+{
+};
+
+int main()
+{ 
+	try
+	{
+		int a;
+		a = 5;
+		throw a;
+	}
+	catch (int & e)
+	{
+		cout << e ;
+	}
+	catch (float & e)
+	{
+		cout << e ;
+	}
+	try
+	{
+		Test a;
+		throw a;
+	}
+	catch (Base & e)
+	{
+		cout << e.m_b;
+	}
+	system("pausse");
+	return 0;
+}

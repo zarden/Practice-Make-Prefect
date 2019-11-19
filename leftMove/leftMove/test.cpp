911fcce8fb424da9fdf621c9c99b7ae7445bffd4
zarden <y1352366024@yeah.net>
2019-11-19 22:28:53 +0800

    leftMove

--- leftMove/leftMove/test.cpp
+++ leftMove/leftMove/test.cpp
@@ -1,39 +1,59 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
-class Base
+int findBreakPoint(const vector<int> &src)
 {
-public:	int m_b;
-};
-class Test:public Base
+	int left = 0, right = src.size() - 1, mid;
+
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		if (src[mid]>src[mid + 1])
+		{
+			return mid + 1;
+		}
+
+		if (src[left]>src[mid])
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+}
+
+int bfind(const vector<int> &src, int bp, int x)
 {
-};
+	int size = src.size();
+	int left = bp, right = bp + size - 1;
+	int mid;
+
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		if (src[mid%size]>x)
+		{
+			right = mid - 1;
+		}
+		else if (src[mid%size]<x)
+		{
+			left = mid + 1;
+		}
+		else
+		{
+			return mid%size;
+		}
+	}
+}
 
 int main()
-{ 
-	try
-	{
-		int a;
-		a = 5;
-		throw a;
-	}
-	catch (int & e)
-	{
-		cout << e ;
-	}
-	catch (float & e)
-	{
-		cout << e ;
-	}
-	try
-	{
-		Test a;
-		throw a;
-	}
-	catch (Base & e)
-	{
-		cout << e.m_b;
-	}
-	system("pausse");
+{
+	vector<int> src{ 6, 1, 2, 3, 4, 5 };
+	int bp = findBreakPoint(src);
+	cout << bfind(src, bp, 6);
+	system("pause");
 	return 0;
 }
